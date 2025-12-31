fe2c0de0b51046a571831960f5fa357652952901
sagarsingh14323-lgtm <sagarsingh14323@gmail.com>
2025-12-31 19:58:04 +0530

    Create splash.dart

---
+++ lib/splash.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/images/your_logo.png', // ← yahan apna logo filename
+          width: 180,
+        ),
+      ),
+    );
+  }
+}
